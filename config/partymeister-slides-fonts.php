78a78b357428a7bc1d03af5d6fc14ccaf3269e9f
Stephan Maihöfer <stephan@undev.de>
2021-08-01 17:48:13 +0200

    add slidefonts

--- config/partymeister-slides-fonts.php
+++ config/partymeister-slides-fonts.php
@@ -2,6 +2,21 @@
 
 return [
     'fonts' => [
+	    [
+		    'name'   => 'motivasans',
+		    'path'   => '/fonts/motivasans.css',
+		    'family' => '\'Motiva Sans\''
+	    ],
+	    [
+		    'name'   => 'tisabold',
+		    'path'   => '/fonts/tisabold.css',
+		    'family' => '\'Tisa Bold\''
+	    ],
+	    [
+		    'name'   => 'tisaregular',
+		    'path'   => '/fonts/tisaregular.css',
+		    'family' => '\'Tisa Regular\''
+	    ],
         [
             'name'   => 'exo2',
             'path'   => '/fonts/exo2.css',
