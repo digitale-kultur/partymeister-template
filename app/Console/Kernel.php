e94a8a4925e991f7846df0eabd16e1cc94285d20
Stephan Maihöfer <stephan@undev.de>
2022-07-28 14:27:21 +0200

    disable crontasks for deinetickets and timetable

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,8 +28,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('partymeister:competitions:link-entry-files')->everyMinute();
         $schedule->command('partymeister:competitions:publish-release-files')->everyMinute();
         $schedule->command('partymeister:frontend:cache-photowall')->everyFiveMinutes();
-        $schedule->command('partymeister:core:import:tickets:api')->everyFiveMinutes();
-        $schedule->command('partymeister:core:import:timetable')->everyFiveMinutes();
+        // $schedule->command('partymeister:core:import:tickets:api')->everyFiveMinutes();
+        // $schedule->command('partymeister:core:import:timetable')->everyFiveMinutes();
     }
 
     /**
