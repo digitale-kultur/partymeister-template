9663d5e24414951834cf8f44b34863fd644cb9b6
potibm <poti@kaoz.org>
2022-08-03 11:27:39 +0200

    Fix: make it a little configurable

--- app/Competitions/PDF/PrizeLabel.php
+++ app/Competitions/PDF/PrizeLabel.php
@@ -5,22 +5,26 @@ namespace App\Competitions\PDF;
 use Partymeister\Competitions\Models\Competition;
 use Partymeister\Competitions\Models\CompetitionPrize;
 use Partymeister\Competitions\Models\Entry;
+use Uskur\PdfLabel\PdfLabel;
 
 
-class PrizeLabel extends \Uskur\PdfLabel\PdfLabel
+class PrizeLabel extends PdfLabel
 {
     public function __construct()
     {
-        parent::__construct('NewPrint4005');
+        parent::__construct(config('partymeister-competitions-prizelabels.format', 'NewPrint4005'));
         $this->addPage();
-        $this->drawCutLines();
     }
 
-    public function addSingleLabel(Competition $competition, Entry $entry, CompetitionPrize $prize) {
-        $text  = '<h4>'. $competition->name . ' #'.$prize->rank . "</h4>";
-        $text .= '<p>"'.$entry->title.'" by '.$entry->author . "</p>";
-        $text .= '<h2>' . $prize->amount . ' EURO</h2>';
-
-        $this->addHtmlLabel($text);
+    public function addSingleLabel(Competition $competition, Entry $entry, CompetitionPrize $prize)
+    {
+        $this->addHtmlLabel(view('backend/competition_prizes/pdf/label', [
+            'competition_name' => $competition->name,
+            'rank' => $prize->rank,
+            'entry_title' => $entry->title,
+            'entry_author' => $entry->author,
+            'prize_amount' => $prize->amount,
+            'prize_currency' => config('partymeister-competitions-receipt.currency')
+        ]));
     }
 }
