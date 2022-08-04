217854fe1173dafbfa51bc159de7546391970089
potibm <poti@kaoz.org>
2022-08-04 21:44:10 +0200

    Fix: modify label config for evoke 2022

--- config/partymeister-competitions-prizelabels.php
+++ config/partymeister-competitions-prizelabels.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    'format' => 'NewPrint4005'
+    'format' => array(
+        'paper-size' => 'A4',
+        'unit' => 'mm',
+        'marginLeft' => 0,
+        'marginTop' => 5,
+        'NX' => 2,
+        'NY' => 5,
+        'SpaceX' => 0,
+        'SpaceY' => 0,
+        'width' => 105,
+        'height' => 57,
+        'cutLines' => true
+    )
 ];
