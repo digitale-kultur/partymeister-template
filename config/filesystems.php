3c4abdc4701d691f3748fb5e6e83bd0898ae9885
Reza Esmaili <bitbucket@dfox.info>
2023-03-29 00:36:35 +0200

    Overwrite locking filesystem for persistent storage on k8s on DO

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,36 +32,48 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root'   => storage_path('app'),
+            'throw'  => false,
+            'lock'   => LOCK_SH,
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'throw'      => false,
+            'lock'       => LOCK_SH,
         ],
 
         'photowall' => [
-            'driver' => 'local',
-            'root'   => storage_path('app/public/photowall'),
-            'url' => env('APP_URL').'/photowall',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public/photowall'),
+            'url'        => env('APP_URL').'/photowall',
             'visibility' => 'public',
+            'throw'      => false,
+            'lock'       => LOCK_SH,
         ],
 
         'media' => [
-            'driver' => 'local',
-            'root'   => storage_path('app/public/media'),
-            'url' => env('APP_URL').'/media',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public/media'),
+            'url'        => env('APP_URL').'/media',
             'visibility' => 'public',
+            'throw'      => false,
+            'lock'       => LOCK_SH,
         ],
 
         's3' => [
-            'driver' => 's3',
-            'key'    => 'your-key',
-            'secret' => 'your-secret',
-            'region' => 'your-region',
-            'bucket' => 'your-bucket',
+            'driver'                  => 's3',
+            'key'                     => env('AWS_ACCESS_KEY_ID'),
+            'secret'                  => env('AWS_SECRET_ACCESS_KEY'),
+            'region'                  => env('AWS_DEFAULT_REGION'),
+            'bucket'                  => env('AWS_BUCKET'),
+            'url'                     => env('AWS_URL'),
+            'endpoint'                => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw'                   => false,
         ],
 
     ],
@@ -78,9 +90,9 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage')   => storage_path('app/public'),
         public_path('photowall') => storage_path('app/public/photowall'),
-        public_path('media') => storage_path('app/public/media'),
+        public_path('media')     => storage_path('app/public/media'),
     ],
 
 ];
--- config/partymeister-core-dt.php
+++ config/partymeister-core-dt.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-    'base_url' => env('DT_BASE_URL'),
-    'email'    => env('DT_EMAIL'),
-    'password' => env('DT_PASSWORD'),
-    'shop'     => env('DT_SHOP'),
+    'base_url'          => env('DT_BASE_URL'),
+    'email'             => env('DT_EMAIL'),
+    'password'          => env('DT_PASSWORD'),
+    'shop'              => env('DT_SHOP'),
+    'ticket_scan_url'   => env('DT_TICKET_SCAN_URL'),
+    'ticket_scan_token' => env('DT_TICKET_SCAN_TOKEN'),
 ];
