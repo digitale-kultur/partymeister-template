6c82bb6c0a33fc703dfd86ecbedf22ec9bd4b7cf
PartyMeister <stephan@5711.org>
2023-08-03 07:38:07 +0000

    on-site changes

--- config/partymeister-core-visitor-registration.php
+++ config/partymeister-core-visitor-registration.php
@@ -7,7 +7,7 @@ return [
         //'NI' => 'Niger',
         //'VI' => 'U.S. Virgin Islands'
     ],
-    'password_reset_from_email' => 'hotline-2023@revision-party.net',
-    'password_reset_from_name' => 'Revision',
-    'password_reset_subject_prefix' => '[Revision 2023] ',
+    'password_reset_from_email' => 'partymeister@digitalekultur.org',
+    'password_reset_from_name' => 'Evoke',
+    'password_reset_subject_prefix' => '[Evoke 2023] ',
 ];
